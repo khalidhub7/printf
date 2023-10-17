128ec2e3bcb673990d2f8e03c4bccdf11692e3ed
khalidhub7 <louguikhalid7@gmail.com>
2023-10-17 12:11:04 -0700

    up

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int specifier_case(char c, va_list args);
+int string_case(char *str);
+int number_case(int numb);
+int number_len(int number);
+
+#endif
