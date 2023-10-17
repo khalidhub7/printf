196a4202b69f23ce03ae8ed4ad178ed3b37cfff9
khalidhub7 <louguikhalid7@gmail.com>
2023-10-17 13:27:32 -0700

    up

--- _printf.c
+++ _printf.c
@@ -7,36 +7,36 @@
  * 
  * Return: number of char printed
  */
- 
+
 int _printf(const char *format, ...)
 {
-        int counter = 0;
-        va_list args;
+	int counter = 0;
+	va_list args;
 
-        va_start(args, format);
+	va_start(args, format);
 
-        if (!format[0] || !format) 
-        {
-            return (-1);
-        }
-	if (format[0] =='%' && format[1] ==' ')
+	if (!format[0] || !format) 
 	{
 		return (-1);
 	}
-        while (*format)
-        {
-                if (*format == '%')
-                {
-                        format++;
-                        counter += specifier_case(*format, args);
-                        format++;
-                }
-                else
-                {
-                        counter += _putchar(*format);
-                        format++;
-                }
-        }
-        va_end(args);
-        return (counter);
+	if (format[0] == '%' && format[1] == ' ')
+	{
+		return (-1);
+	}
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			counter += specifier_case(*format, args);
+			format++;
+		}
+		else
+		{
+			counter += _putchar(*format);
+			format++;
+		}
+	}
+	va_end(args);
+	return (counter);
 }
