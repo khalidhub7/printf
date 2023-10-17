8c0183f1f2a9b7a3eb6359a95e8330377ff4b4f9
zakorca <zakariatouras47.com>
2023-10-17 13:54:17 -0700

    add main.c for checkin

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+#include <limits.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+    int len;
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+
+        _printf("Percent:[%%]\n");
+        _printf("Character:[%c]\n", 'H');
+        _printf("String:[%s]\n", "I am a string !");
+        _printf("Unknown:[%r]\n");
+        _printf("%ok %r %% \n");
+        _printf("Length:[%d, %i]\n", len, len);
+        _printf("Negative:[%d]\n", -762534);
+        _printf("Len:[%d]\n", len);
+        _printf("%r  \n");
+        printf("printf %% \n");
+        _printf("_printf %% \n");
+        printf("%%  \n");
+        _printf("%%  \n");
+        return (0);
+}
